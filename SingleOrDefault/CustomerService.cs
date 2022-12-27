e3743ee757191ac109aef4650a02c8a758c6ffdd
Cesar Lopez <cesarlerdeut@gmail.com>
2022-12-27 20:32:34 +0100

    Add Customers with Bogus

--- SingleOrDefault/CustomerService.cs
+++ SingleOrDefault/CustomerService.cs
@@ -1,9 +1,23 @@
+using Bogus;
+
 namespace SingleOrDefault;
 
 public class CustomerService
 {
     private readonly List<Customer> _customers = new();
 
+    public CustomerService()
+    {
+        Randomizer.Seed = new Random(69);
+        var customerFaker = new Faker<Customer>()
+            .RuleFor(x => x.Id, faker => faker.IndexFaker)
+            .RuleFor(x => x.FullName, faker => faker.Person.FullName)
+            .RuleFor(x => x.Email, faker => faker.Person.Email)
+            .RuleFor(x => x.DateOfBirth, faker => faker.Person.DateOfBirth);
+
+        _customers = customerFaker.Generate(10000);
+
+    }
     public Customer? GetById_SingleOrDefault(int id)
     {
         return _customers.SingleOrDefault(x => x.Id == id);
